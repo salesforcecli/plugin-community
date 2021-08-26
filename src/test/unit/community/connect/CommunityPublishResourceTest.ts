3be494e212fbb59c29e81d0b5c9c702889025813
Eric Willhoit <ewillhoit@salesforce.com>
2021-08-26 15:48:01 -0500

    chore: copy code from toolbelt

---
+++ src/test/unit/community/connect/CommunityPublishResourceTest.ts
@@ -0,0 +1,135 @@
+/*
+ * Copyright (c) 2020, salesforce.com, inc.
+ * All rights reserved.
+ * Licensed under the BSD 3-Clause license.
+ * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+ */
+import { CommunitiesServices } from '../../../../lib/community/service/CommunitiesServices';
+import { CommunityPublishResource } from '../../../../lib/community/connect/CommunityPublishResource';
+import { Org } from '@salesforce/core/lib/org';
+import { UX } from '@salesforce/command/lib/ux';
+import { CommunityPublishResponse } from '../../../../lib/community/defs/CommunityPublishResponse';
+import { JsonCollection } from '@salesforce/ts-types';
+import { Messages } from '@salesforce/core';
+import util = require('util');
+
+const sinon = require('sinon');
+const chai = require('chai');
+const expect = chai.expect;
+chai.use(require('chai-as-promised'));
+
+Messages.importMessagesDirectory(__dirname);
+const communityMessages = Messages.loadMessages('salesforce-alm', 'community_commands');
+
+describe('CommunityPublishResource', () => {
+  let org: Org;
+  let communityPublishResource: CommunityPublishResource;
+  const communityName = 'communityName';
+  const validCommunityId = '0DB0000000000';
+  let table;
+  let styledHeader;
+
+  before(() => {
+    org = new Org(null);
+    table = sinon.stub(UX.prototype, 'table');
+    styledHeader = sinon.stub(UX.prototype, 'styledHeader');
+    communityPublishResource = getCommunityPublishResource();
+  });
+
+  after(() => {
+    table.restore();
+    styledHeader.restore();
+  });
+
+  describe('fetchRelativeConnectUrl', () => {
+    let communitiesServices;
+    beforeEach(() => {
+      communitiesServices = sinon.stub(CommunitiesServices, 'fetchCommunityInfoFromName');
+    });
+    afterEach(() => {
+      communitiesServices.restore();
+    });
+
+    it('should throw when invalid id is given', async () => {
+      communitiesServices.returns(Promise.resolve(undefined));
+      await expect(communityPublishResource.fetchRelativeConnectUrl()).to.be.rejectedWith(
+        util.format(communityMessages.getMessage('publish.error.communityNotExists'), communityName)
+      );
+    });
+
+    it('should return relative url with community id', async () => {
+      communitiesServices.returns(
+        Promise.resolve({
+          id: validCommunityId,
+        })
+      );
+      expect(await communityPublishResource.fetchRelativeConnectUrl()).to.equal(
+        `/connect/communities/${validCommunityId}/publish`
+      );
+    });
+  });
+
+  describe('getRequestMethod', () => {
+    it('should POST', () => {
+      expect(communityPublishResource.getRequestMethod()).to.equal('POST');
+    });
+  });
+
+  describe('fetchPostParams', () => {
+    it('should not have any post params', async () => {
+      expect(await communityPublishResource.fetchPostParams()).to.equal('{}');
+    });
+  });
+
+  describe('handleSuccess', () => {
+    let communitiesServices;
+    beforeEach(() => {
+      communitiesServices = sinon.stub(CommunitiesServices, 'fetchCommunityInfoFromName');
+    });
+    afterEach(() => {
+      communitiesServices.restore();
+    });
+
+    it('should return community info', async () => {
+      communitiesServices.returns(
+        Promise.resolve({
+          id: validCommunityId,
+          status: 'UnderConstruction',
+        })
+      );
+      const connectResponse: JsonCollection = {
+        id: validCommunityId,
+        message: 'message',
+        name: communityName,
+        url: 'http://someurl.com/s',
+      };
+      // This sets up CommunityInfo
+      await communityPublishResource.fetchCommunityId();
+      const result: CommunityPublishResponse = communityPublishResource.handleSuccess(connectResponse);
+      expect(result.id).to.equal(validCommunityId);
+      expect(result.message).to.equal(
+        'We’re publishing your changes now. You’ll receive an email confirmation when your changes are live.'
+      );
+      expect(result.name).to.equal(communityName);
+      expect(result.status).to.equal('UnderConstruction');
+      expect(result.url.toString()).to.equal('http://someurl.com/s');
+    });
+  });
+
+  describe('handleError', () => {
+    it('should throw an error', () => {
+      const errorMsg = 'An Error Occured';
+      expect(() => communityPublishResource.handleError(new Error(errorMsg))).to.throw(errorMsg);
+    });
+  });
+
+  function getCommunityPublishResource(): CommunityPublishResource {
+    return new CommunityPublishResource(
+      {
+        name: communityName,
+      },
+      org,
+      new UX(null)
+    );
+  }
+});
