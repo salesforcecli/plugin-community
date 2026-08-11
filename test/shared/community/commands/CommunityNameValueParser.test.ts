220725f18fedec6cd50707ff7a34512b24b8e4c5
Willie Ruemmele <willieruemmele@gmail.com>
2026-08-11 09:35:58 -0600

    chore: eslint 10 upgrade fixes @W-23474629@

--- test/shared/community/commands/CommunityNameValueParser.test.ts
+++ test/shared/community/commands/CommunityNameValueParser.test.ts
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
- 
 
 import { expect, assert } from 'chai';
 import { Config } from '@oclif/core';
